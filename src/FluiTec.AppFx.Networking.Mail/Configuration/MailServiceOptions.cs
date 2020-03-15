1c98636b4ba9dcabc5d09cd3a9891f1be342ca2a
Achim Schnell <a.schnell@wtschnell.de>
2020-03-15 12:59:05 +0100

    Add MailServiceOptions and basic extension

---
+++ src/FluiTec.AppFx.Networking.Mail/Configuration/MailServiceOptions.cs
@@ -0,0 +1,49 @@
+﻿using FluiTec.AppFx.Options.Attributes;
+
+namespace FluiTec.AppFx.Networking.Mail.Configuration
+{
+    /// <summary>Options for a mail-service.</summary>
+    [ConfigurationKey("MailServiceOptions")]
+    public class MailServiceOptions
+    {
+        /// <summary>Initializes a new instance of the <see cref="MailServiceOptions"/> class.</summary>
+        public MailServiceOptions()
+        {
+            Authenticate = false;
+            SmtpPort = 25;
+            EnableSsl = false;
+        }
+
+        /// <summary>	Gets or sets a value indicating whether the authenticate. </summary>
+        /// <value>	True if authenticate, false if not. </value>
+        public bool Authenticate { get; set; }
+
+        /// <summary>	Gets or sets the SMTP server. </summary>
+        /// <value>	The SMTP server. </value>
+        public string SmtpServer { get; set; }
+
+        /// <summary>	Gets or sets the SMTP port. </summary>
+        /// <value>	The SMTP port. </value>
+        public int SmtpPort { get; set; }
+
+        /// <summary>	Gets or sets a value indicating whether the ssl is enabled. </summary>
+        /// <value>	True if enable ssl, false if not. </value>
+        public bool EnableSsl { get; set; }
+
+        /// <summary>	Gets or sets the username. </summary>
+        /// <value>	The username. </value>
+        public string Username { get; set; }
+
+        /// <summary>	Gets or sets the password. </summary>
+        /// <value>	The password. </value>
+        public string Password { get; set; }
+
+        /// <summary>	Gets or sets from mail. </summary>
+        /// <value>	from mail. </value>
+        public string FromMail { get; set; }
+
+        /// <summary>	Gets or sets the name of from. </summary>
+        /// <value>	The name of from. </value>
+        public string FromName { get; set; }
+    }
+}
---
+++ src/FluiTec.AppFx.Networking.Mail/Configuration/Validators/MailServiceOptionsValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace FluiTec.AppFx.Networking.Mail.Configuration.Validators
+{
+    /// <summary>Validator for MailServiceOptions</summary>
+    /// <seealso cref="FluentValidation.AbstractValidator{MailServiceOptions}" />
+    public class MailServiceOptionsValidator : AbstractValidator<MailServiceOptions>
+    {
+        /// <summary>Initializes a new instance of the <see cref="MailServiceOptionsValidator"/> class.</summary>
+        public MailServiceOptionsValidator()
+        {
+            RuleFor(options => options.SmtpServer).NotEmpty();
+            RuleFor(options => options.SmtpPort).GreaterThan(0);
+            RuleFor(options => options.FromMail).NotEmpty();
+            RuleFor(options => options.FromName).NotEmpty();
+            RuleFor(options => options.Username).NotEmpty()
+                .When(options => options.Authenticate);
+            RuleFor(options => options.Password).NotEmpty()
+                .When(options => options.Authenticate);
+        }
+    }
+}
