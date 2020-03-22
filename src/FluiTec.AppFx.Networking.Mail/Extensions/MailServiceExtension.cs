b2ae94b7697fa90f2aa8ebe9163514f934cd1411
Achim Schnell <a.schnell@wtschnell.de>
2020-03-22 09:48:34 +0100

    ...

--- src/FluiTec.AppFx.Networking.Mail/Extensions/MailServiceExtension.cs
+++ src/FluiTec.AppFx.Networking.Mail/Extensions/MailServiceExtension.cs
@@ -1,6 +1,13 @@
 ﻿using FluiTec.AppFx.Networking.Mail.Configuration;
 using FluiTec.AppFx.Networking.Mail.Configuration.Validators;
+using FluiTec.AppFx.Networking.Mail.RazorLightExtensions.LocationExpanders;
+using FluiTec.AppFx.Networking.Mail.RazorLightExtensions.Projects;
+using FluiTec.AppFx.Networking.Mail.Services;
 using FluiTec.AppFx.Options.Managers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using RazorLight;
+using RazorLight.Razor;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection
@@ -19,6 +26,7 @@ namespace Microsoft.Extensions.DependencyInjection
             // configure options and validators
             manager.ConfigureValidator(new MailServiceOptionsValidator());
             services.Configure<MailServiceOptions>(manager);
+            services.AddScoped<IMailService, ConfigurationValidatingMailService>();
             return services;
         }
 
@@ -32,19 +40,51 @@ namespace Microsoft.Extensions.DependencyInjection
             // configure options and validators
             manager.ConfigureValidator(new MailTemplateOptionsValidator());
             services.Configure<MailTemplateOptions>(manager);
+            services.AddScoped<ITemplatingService, RazorLightTemplatingService>();
+            return services;
+        }
 
+        /// <summary>Configures the razor light.</summary>
+        /// <param name="services">The services.</param>
+        /// <param name="environment">The environment.</param>
+        /// <returns></returns>
+        public static IServiceCollection ConfigureRazorLight(this IServiceCollection services,
+            IHostingEnvironment environment)
+        {
+            services.AddSingleton<ILocationExpander, DefaultCultureLocationExpander>();
+            services.AddSingleton<ILocationExpander, SharedCultureLocationExpander>();
+            services.AddSingleton<ILocationExpander, SharedLocationExpander>();
+            services.AddSingleton<ILocationExpander, DefaultLocationExpander>();
+            services.AddSingleton<RazorLightProject>(provider =>
+            {
+                var expanders = provider.GetServices<ILocationExpander>();
+                var templateOptions = provider.GetRequiredService<MailTemplateOptions>();
+                var absoluteRoot = System.IO.Path.Combine(environment.ContentRootPath, templateOptions.BaseDirectory);
+                return new LocationExpandingRazorProject(templateOptions, expanders,
+                    provider.GetService<ILogger<LocationExpandingRazorProject>>());
+            });
+            services.AddSingleton<IRazorLightEngine>(provider =>
+            {
+                var project = provider.GetRequiredService<RazorLightProject>();
+                return new RazorLightEngineBuilder()
+                    .UseProject(project)
+                    .UseMemoryCachingProvider()
+                    .Build();
+            });
             return services;
         }
 
         /// <summary>Configures the mail service and templates.</summary>
         /// <param name="services">The services.</param>
+        /// <param name="environment">The HostingEnvironment.</param>
         /// <param name="manager">The manager.</param>
         /// <returns></returns>
         // ReSharper disable once UnusedMember.Global
-        public static IServiceCollection ConfigureMailServiceTemplated(this IServiceCollection services,
-            ValidatingConfigurationManager manager)
+        public static IServiceCollection ConfigureMailServiceTemplated(this IServiceCollection services, 
+            IHostingEnvironment environment, ValidatingConfigurationManager manager)
         {
             return services
+                .ConfigureRazorLight(environment)
                 .ConfigureMailService(manager)
                 .ConfigureMailTemplateService(manager);
         }
---
+++ src/FluiTec.AppFx.Networking.Mail/RazorLightExtensions/Projects/LocationExpandingRazorProject.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using FluiTec.AppFx.Networking.Mail.Configuration;
+using FluiTec.AppFx.Networking.Mail.RazorLightExtensions.LocationExpanders;
+using Microsoft.Extensions.Logging;
+using RazorLight.Razor;
+
+namespace FluiTec.AppFx.Networking.Mail.RazorLightExtensions.Projects
+{
+    /// <summary>A location expanding razor project.</summary>
+    public class LocationExpandingRazorProject : FileSystemRazorProject
+    {
+        #region Fields
+
+        /// <summary>The root.</summary>
+        private readonly MailTemplateOptions _mailTemplateOptions;
+
+        /// <summary>The expanders.</summary>
+        private readonly IEnumerable<ILocationExpander> _expanders;
+
+        /// <summary>   The logger. </summary>
+        private readonly ILogger<LocationExpandingRazorProject> _logger;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>   Constructor. </summary>
+        /// <exception cref="ArgumentNullException">    Thrown when one or more required arguments are
+        ///                                             null. </exception>
+        /// <param name="templateOptions">  The templateOptions.</param>
+        /// <param name="expanders">        The expanders. </param>
+        /// <param name="logger">           The logger. </param>
+        public LocationExpandingRazorProject(MailTemplateOptions templateOptions, IEnumerable<ILocationExpander> expanders, ILogger<LocationExpandingRazorProject> logger) 
+            : base(templateOptions.BaseDirectory)
+        {
+            _mailTemplateOptions = templateOptions;
+            _expanders = expanders ?? throw new ArgumentNullException(nameof(expanders));
+            _logger = logger;
+        }
+
+        #endregion
+
+        #region FileSystemRazorProject
+
+        /// <summary>Gets item asynchronous.</summary>
+        /// <param name="templateKey">  The template key. </param>
+        /// <returns>The asynchronous result that yields the item asynchronous.</returns>
+        public override Task<RazorLightProjectItem> GetItemAsync(string templateKey)
+        {
+            foreach (var expander in _expanders)
+                foreach (var location in expander.Expand(templateKey))
+                {
+                    var absolutePath = NormalizeKey(location);
+                    _logger.LogInformation($"Trying to find MailTemplate {absolutePath}.");
+                    if (File.Exists(absolutePath))
+                        return Task.FromResult(
+                            (RazorLightProjectItem)new FileSystemRazorProjectItem(location,
+                                new FileInfo(absolutePath)));
+                }
+
+            // let the base-class try it's best and throw...
+            return base.GetItemAsync(templateKey);
+        }
+
+        #endregion
+    }
+}
