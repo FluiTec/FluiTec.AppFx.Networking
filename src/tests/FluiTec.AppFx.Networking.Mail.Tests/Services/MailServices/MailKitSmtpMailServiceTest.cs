f934f8a3818dfa990ed311877e04de2bb0f31fae
Achim Schnell <a.schnell@wtschnell.de>
2020-03-24 17:00:25 +0100

    ...

--- src/tests/FluiTec.AppFx.Networking.Mail.Tests/Services/MailServices/MailKitSmtpMailServiceTest.cs
+++ src/tests/FluiTec.AppFx.Networking.Mail.Tests/Services/MailServices/MailKitSmtpMailServiceTest.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using FluiTec.AppFx.Networking.Mail.Configuration;
+using FluiTec.AppFx.Networking.Mail.Tests.Mocking;
 using FluiTec.AppFx.Networking.Mail.Tests.Services.MailServices.TestServices;
 using FluiTec.AppFx.Options.Exceptions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MimeKit.Text;
 
 namespace FluiTec.AppFx.Networking.Mail.Tests.Services.MailServices
 {
@@ -12,14 +14,6 @@ namespace FluiTec.AppFx.Networking.Mail.Tests.Services.MailServices
         protected const string SmtpMail = "test@example.com";
         protected const string SmtpName = "Test";
         protected const string SmtpServer = "127.0.0.1";
-
-        private int _lastPort = 49999;
-
-        protected int GetSmtpPort()
-        {
-            _lastPort++;
-            return _lastPort;
-        }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
@@ -33,6 +27,20 @@ namespace FluiTec.AppFx.Networking.Mail.Tests.Services.MailServices
         public void ThrowsOnInvalidOptions()
         {
             var unused = new TestMailKitSmtpMailService(new MailServiceOptions {SmtpServer = "127.0.0.1"});
+        }
+
+        [TestMethod]
+        public void CanSendMail()
+        {
+            var smtpMock = new SmtpMock(25, "example.com");
+            var service = new TestMailKitSmtpMailService(new MailServiceOptions
+            {
+                FromMail = "test@example.com",
+                FromName = "Test",
+                SmtpServer = SmtpServer,
+                SmtpPort = 25
+            });
+            service.SendEmail("test@example.com", "TestSubject", "TestContent", TextFormat.Plain, "Test");
         }
     }
 }
