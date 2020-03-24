1447f33606c4e579a46a49b2bc21210407f324e4
Achim Schnell <a.schnell@wtschnell.de>
2020-03-24 21:15:55 +0100

    ...

--- src/tests/FluiTec.AppFx.Networking.Mail.Tests/Services/MailServices/MailKitSmtpMailServiceTest.cs
+++ src/tests/FluiTec.AppFx.Networking.Mail.Tests/Services/MailServices/MailKitSmtpMailServiceTest.cs
@@ -61,27 +61,27 @@ namespace FluiTec.AppFx.Networking.Mail.Tests.Services.MailServices
             var unused = new TestMailKitSmtpMailService(new MailServiceOptions {SmtpServer = "127.0.0.1"});
         }
 
-        [TestMethod]
-        public void MailTest()
-        {
-            var server = SimpleSmtpServer.Start(25);
+        //[TestMethod]
+        //public void MailTest()
+        //{
+        //    var server = SimpleSmtpServer.Start(25);
 
-            using (var client = new SmtpClient())
-            {
-                client.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true; // accepts all certs
-                client.Connect("127.0.0.1", 25, false);
-                client.AuthenticationMechanisms.Remove("XOAUTH2");
-                client.Send(new MimeMessage
-                {
-                    From = { new MailboxAddress("Test", "test@example.com") },
-                    To = { new MailboxAddress("Test", "test@example.com") },
-                    Subject = "Subject",
-                    Body = new TextPart(TextFormat.Plain)
-                });
-            }
+        //    using (var client = new SmtpClient())
+        //    {
+        //        client.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true; // accepts all certs
+        //        client.Connect("127.0.0.1", 25, false);
+        //        client.AuthenticationMechanisms.Remove("XOAUTH2");
+        //        client.Send(new MimeMessage
+        //        {
+        //            From = { new MailboxAddress("Test", "test@example.com") },
+        //            To = { new MailboxAddress("Test", "test@example.com") },
+        //            Subject = "Subject",
+        //            Body = new TextPart(TextFormat.Plain)
+        //        });
+        //    }
 
-            server.Stop();
-        }
+        //    server.Stop();
+        //}
 
         //[TestMethod]
         //public void CanSendMail2()
