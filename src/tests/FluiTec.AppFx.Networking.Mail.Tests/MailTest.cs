c722712b86463a0d39d7ab0ac66878d2f7fa0f13
Achim Schnell <a.schnell@wtschnell.de>
2020-03-25 09:04:13 +0100

    Trying second mailtest

--- src/tests/FluiTec.AppFx.Networking.Mail.Tests/MailTest.cs
+++ src/tests/FluiTec.AppFx.Networking.Mail.Tests/MailTest.cs
@@ -38,33 +38,32 @@ namespace FluiTec.AppFx.Networking.Mail.Tests
             server.Stop();
         }
 
-        //[TestMethod]
-        //public void DoMailTest2()
-        //{
-        //    var port = 25;
-        //    var server = SimpleSmtpServer.Start(port);
-        //    var service = new TestMailKitSmtpMailService(new MailServiceOptions
-        //    {
-        //        FromMail = GlobalTestSettings.SmtpMail,
-        //        FromName = GlobalTestSettings.SmtpName,
-        //        SmtpPort = port,
-        //        SmtpServer = "127.0.0.1"
-        //    });
-        //    service.SendEmail(GlobalTestSettings.SmtpMail, GlobalTestSettings.MailSubject,
-        //        GlobalTestSettings.MailContent, TextFormat.Plain, GlobalTestSettings.SmtpName);
-        //    server.Stop();
+        [TestMethod]
+        public void DoMailTest2()
+        {
+            var port = 25;
+            var server = SimpleSmtpServer.Start(port);
+            var service = new TestMailKitSmtpMailService(new MailServiceOptions
+            {
+                FromMail = GlobalTestSettings.SmtpMail,
+                FromName = GlobalTestSettings.SmtpName,
+                SmtpPort = port,
+                SmtpServer = "127.0.0.1"
+            });
+            service.SendEmail(GlobalTestSettings.SmtpMail, GlobalTestSettings.MailSubject,
+                GlobalTestSettings.MailContent, TextFormat.Plain, GlobalTestSettings.SmtpName);
 
-        //    var email = server.ReceivedEmail.Single();
-        //    Assert.AreEqual(GlobalTestSettings.SmtpMail, email.To.Single().Address);
-        //    Assert.AreEqual(GlobalTestSettings.SmtpName, email.To.Single().DisplayName);
+            var email = server.ReceivedEmail.Single();
+            Assert.AreEqual(GlobalTestSettings.SmtpMail, email.To.Single().Address);
+            Assert.AreEqual(GlobalTestSettings.SmtpName, email.To.Single().DisplayName);
 
-        //    Assert.AreEqual(GlobalTestSettings.SmtpMail, email.From.Address);
-        //    Assert.AreEqual(GlobalTestSettings.SmtpName, email.From.DisplayName);
+            Assert.AreEqual(GlobalTestSettings.SmtpMail, email.From.Address);
+            Assert.AreEqual(GlobalTestSettings.SmtpName, email.From.DisplayName);
 
-        //    Assert.AreEqual(GlobalTestSettings.MailSubject, email.Subject);
-        //    Assert.AreEqual(GlobalTestSettings.MailContent, email.Body);
+            Assert.AreEqual(GlobalTestSettings.MailSubject, email.Subject);
+            Assert.AreEqual(GlobalTestSettings.MailContent, email.Body);
 
-        //    server.Stop();
-        //}
+            server.Stop();
+        }
     }
 }
